aac246b5b7f6c03ef2151d51361b8e79c7726206
Hira Iqbal <hirach502@gmail.com>
2022-04-18 13:26:43 +0500

    using async/await instead of fetch and then

--- src/App.js
+++ src/App.js
@@ -6,21 +6,19 @@ import './App.css';
 function App() {
 
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films').then(response=>{
-      return response.json().then(data => {
+  async function fetchMoviesHandler() {
+    const response = await fetch('https://swapi.dev/api/films')
+    const data = await response.json();
 
-        const transformedMovies = data.results.map(moviedata => {
-          return {
-            id: moviedata.episode_id,
-            title: moviedata.title,
-            openingText: moviedata.opening_crawl,
-            releaseDate: moviedata.release_date
-          };
-        })
-        setMovies(transformedMovies);
-      });
-    });
+    const transformedMovies = data.results.map(moviedata => {
+      return {
+        id: moviedata.episode_id,
+        title: moviedata.title,
+        openingText: moviedata.opening_crawl,
+        releaseDate: moviedata.release_date
+      };
+    })
+    setMovies(transformedMovies);
   }
 
   return (
